dbf37010af3147d4d1c4f81c3d4177582fbe0fa1
Matt Rodrigues <ec.mattrodrigues@gmail.com>
2021-11-21 22:06:45 -0300

    Add Template String
    Nesse exercício foi aprendido a utilizar o template String ao invés da concatenação de strings que era utilizada anteriormente

---
+++ HSM-Coders/JavaScript-Advanced/Template-String/templateString.js
@@ -0,0 +1,8 @@
+// O template String vei com o ECMAScript 6
+// ele serve para substituir a concatenação se strings
+// usa-se `${}` para tamplate string
+
+const name = "Matt";
+const atributo = "estudioso";
+
+console.log(`${name} é um aluno muito ${atributo}`);
